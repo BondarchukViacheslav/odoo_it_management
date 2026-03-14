24d545df4004fdb2d72e3155b8487d385e980c68
xSPx <work.xSPx@gmail.com>
2026-03-14 13:12:20 +0200

    Stage 1: Initializing the module

--- bp_it_consumable/__manifest__.py
+++ bp_it_consumable/__manifest__.py
@@ -1,1 +1,29 @@
-{}
+{
+    'name': 'IT Consumable Management',
+    'version': '19.0.1.0.0',
+    'category': 'IT Management',
+    'summary': 'Облік витратних матеріалів та картриджів відділу IT',
+    'sequence': 10,
+    "author": "Serhii Pidopryhora, "
+    "Odoo Community Association (OCA)",
+    'license': 'LGPL-3',
+    'depends': [
+        'base',
+        'mail',
+        'hr',
+        'bp_it_equipment',
+    ],
+    'data': [
+        # Сюди ми будемо додавати шляхи до файлів security, views, wizards, data тощо.
+    ],
+    'demo': [
+        # 'data/demo_data.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'images': [
+        'static/description/icon.png'
+    ],
+}
+
